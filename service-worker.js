1e770d94e0b59cd31736cee55bba1ab9e5f3b836
haniwa57 <p6614175@gmail.com>
2024-11-24 10:28:36 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -7,6 +7,13 @@ const urlsToCache = [
   "./icon-192.png",
   "./icon-512.png"
 ];
+self.addEventListener("push", event => {
+  const data = event.data.json();
+  self.registration.showNotification(data.title, {
+    body: data.body,
+    icon: "./icons/icon-192x192.png"
+  });
+});
 
 self.addEventListener("install", event => {
   event.waitUntil(
